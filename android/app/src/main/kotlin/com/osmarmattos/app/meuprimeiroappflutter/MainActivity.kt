c4d75a7b29d17a1fe5b0bf15690884c3269e4899
Osmarmattos <osmarmattos777@gmail.com>
2024-09-24 13:53:33 -0300

    Commit inicial

---
+++ android/app/src/main/kotlin/com/osmarmattos/app/meuprimeiroappflutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.osmarmattos.app.meuprimeiroappflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ meuapp/android/app/src/main/kotlin/com/osmarmattos/app/meuapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.osmarmattos.app.meuapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
